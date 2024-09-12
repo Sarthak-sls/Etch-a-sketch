629497cbfb5fee6d0b2df9f74c70631954147a2c
Sarthak-sls <sarthakbiswal111222@gmail.com>
2024-09-12 20:08:40 +0530

    Added feature grid change

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const container=document.querySelector("#grid");
-for (let i = 0; i < 16*16; i++) {
+const container = document.querySelector(".grid-container");
+const gridRange = document.getElementById("gridRange")
+function createGrid(size) {
+    container.innerHTML = ''
+    container.style.gridTemplateColumns=`repeat(${size},1fr)`;
+    container.style.gridTemplateRows=`repeat(${size},1fr)`;
+
+for (let i = 0; i < size*size; i++) {
     const gridItem =document.createElement('div') ;
     gridItem.classList.add('grid-item');
-
     container.appendChild(gridItem);
     gridItem.addEventListener('mouseover',changeColor)
 }
+}
+createGrid(16);
+
 
 function changeColor(event) {
- event.target.style.backgroundColor='black'; 
+    event.target.style.backgroundColor='black'; 
 }
 
 
-
+gridRange.addEventListener("input", (e) => {
+    createGrid(e.target.value)
+})
